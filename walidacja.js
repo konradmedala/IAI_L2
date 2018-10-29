4f0d83d7f1503a57861cf13870f018df3c2a32b2
KamilKafara <=>
2018-10-29 19:51:10 +0100

    eh

---
+++ walidacja.js
@@ -0,0 +1,16 @@
+let  send document.querySelector(',send');
+let  clear document.querySelector(',clear');
+
+send.addEventListener('click',function () {
+        console.log('Przycisk został wciśnięty');
+
+        if()
+
+})
+
+clear.addEventListener('click',function () {
+    console.log('Przycisk został wciśnięty')
+})
+
+
+    
